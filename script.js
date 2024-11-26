9ba59bea1567b07be1eabfd33bcc2d71497bccf7
maruthi0209 <sethumaruthi93@gmail.com>
2024-11-26 20:38:37 +0530

    Initial commit for Task 1

---
+++ script.js
@@ -0,0 +1,36 @@
+// Question 1
+for (var i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+// Question 2
+for (var i = 1; i <= 10; i++) {
+    console.log(i**2);
+}
+
+// Question 3
+for (var i = 1; i <= 10; i++) {
+    console.log(i * 5);
+}
+
+// Question 4
+for (var i = 20; i >= 1; i--) {
+    console.log(i);   
+}
+
+// Question 5
+for (var i = 20; i >= 1; i = i - 2) {
+    console.log(i);
+}
+
+// Question 6
+var arr = [10, 20, 30, 40, 50];
+for (var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+// Question 7
+var arr = ['apple', 'banana', 'cherry'];
+for (var i = 0; i < arr.length; i++) {
+    console.log(`Index ${i} : ${arr[i]}`);
+}
